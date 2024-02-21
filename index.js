e03606849eefe3502398c6975a9732128ee34866
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-02-21 08:20:17 +0100

    4 commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3000
 const { dbConnection,} = require('./config/config');
 const routes = require('./routes');
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use('/', routes);
 //Se ha modificado el import de la const MONGO_URI para que apuntara al .env y se ha insertado en .env las credenciales 
 //de mongodb este punto creo que puede tener el fallo de como se exporta del .env??
 //He movido el bloque de codigo por encima de app.listen para que se pueda escuchar en el 8080
-const { MONGO_URI } = require("./env/.env");
+const { MONGO_URI } = require("./.env");
 const swaggerUI = require('swagger-ui-express')
 const docs = require('./docs/index')
 app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))
